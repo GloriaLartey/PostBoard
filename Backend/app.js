f95662ffd3658f27d94d9e75005935230dd90c7d
Gloria <glorialartey15@gmail.com>
2026-06-17 12:08:15 +0000

    Cors Edit

--- Backend/app.js
+++ Backend/app.js
@@ -25,7 +25,7 @@ const corsOptions = {
   origin:
     process.env.VITE_CLIENT_URL ||
     "http://localhost:5173" ||
-    "https://post-board-coral.vercel.app",
+    "https://post-board-frontend.vercel.app/",
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
